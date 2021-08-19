748aa4294d39e383218f6f770510c82cd6fad97e
Alex <thoms.alexander23@gmail.com>
2021-08-19 00:36:05 -0400

    fix: potential fix to compile error using proper arguments

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,5 +1,5 @@
 // import "./App.css";
-import ExpenseItem from "./compenents/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -36,40 +36,10 @@ function App() {
       date: new Date(2021, 6, 8),
     },
   ];
-
   return (
-    <div className="App">
+    <div>
       <h2>Let's Get Started</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-      <ExpenseItem
-        title={expenses[4].title}
-        amount={expenses[4].amount}
-        date={expenses[4].date}
-      />
-      <ExpenseItem
-        title={expenses[5].title}
-        amount={expenses[5].amount}
-        date={expenses[5].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ expense-tracker/src/components/Expenses.js
@@ -0,0 +1,40 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+
+function Expenses(items) {
+  return (<Card className="expenses">
+    <ExpenseItem
+      title={items[0].title}
+      amount={items[0].amount}
+      date={items[0].date}
+    />
+    <ExpenseItem
+      title={items[1].title}
+      amount={items[1].amount}
+      date={items[1].date}
+    />
+    <ExpenseItem
+      title={items[2].title}
+      amount={items[2].amount}
+      date={items[2].date}
+    />
+    <ExpenseItem
+      title={items[3].title}
+      amount={items[3].amount}
+      date={items[3].date}
+    />
+    <ExpenseItem
+      title={items[4].title}
+      amount={items[4].amount}
+      date={items[4].date}
+    />
+    <ExpenseItem
+      title={items[5].title}
+      amount={items[5].amount}
+      date={items[5].date}
+    />
+  </Card>);
+}
+
+export default Expenses;
