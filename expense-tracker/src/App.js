c8314bbfa15b85b1618c03b1a66a8f8ef0239399
Alex <thoms.alexander23@gmail.com>
2021-08-18 23:15:52 -0400

    feat: added dynamic values using props

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,10 +2,74 @@
 import ExpenseItem from "./compenents/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "1",
+      title: "Delta Airlines",
+      amount: 1312.23,
+      date: new Date(2021, 6, 13),
+    },
+    { id: "2", title: "NYSEG", amount: 55.34, date: new Date(2021, 6, 10) },
+    {
+      id: "3",
+      title: "Grizz Lacrosse",
+      amount: 540.0,
+      date: new Date(2021, 6, 25),
+    },
+    {
+      id: "4",
+      title: "Glacier National Park",
+      amount: 50.0,
+      date: new Date(2021, 6, 20),
+    },
+    {
+      id: "5",
+      title: "Sprinter Loan",
+      amount: 298.88,
+      date: new Date(2021, 6, 16),
+    },
+    { id: "6", title: "Mortgage", amount: 789.99, date: new Date(2021, 6, 14) },
+    {
+      id: "7",
+      title: "Heating Oil",
+      amount: 273.91,
+      date: new Date(2021, 6, 8),
+    },
+  ];
+
   return (
     <div className="App">
       <h2>Let's Get Started</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[4].title}
+        amount={expenses[4].amount}
+        date={expenses[4].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[5].title}
+        amount={expenses[5].amount}
+        date={expenses[5].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- expense-tracker/src/compenents/ExpenseItem.js
+++ expense-tracker/src/compenents/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 6, 10);
-  const expenseTitle = "Delta Airlines";
-  const expenseCost = 3135.7;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseCost}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount.toFixed(2)}</div>
       </div>
     </div>
   );
