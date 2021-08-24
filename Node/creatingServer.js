0395865cdf33a5d0ad542d47f2cbb09a567aced8
Alex <thoms.alexander23@gmail.com>
2021-08-24 15:23:54 -0400

    refactor: cleaned up and organized Node directory and spereated routes using express router

--- Node/creatingServer.js
+++ Node/creatingServer.js
@@ -1,30 +1,14 @@
-const http = require("http");
-
-//old vanilla node.js
-// const routes = require("./routes");
-
 //using express
- const express = require("express");
- const app = express();
+const express = require("express");
+const app = express();
 
-// next moves us to the next app.use
-// use path farther in the app vs starting with root path
- app.use('/add-product',(req,res, next) => {
-  console.log('In the add product path');
-  res.send('<h1>Hello from add product Express</h1>');
- });
+const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
 
- // send ends the flow and sends the response to the browser
- app.use((req,res, next) => {
-  console.log('In the root path');
-  res.send('<h1>Hello from Express</h1>');
- });
+app.use(express.urlencoded({ extended: true }));
 
-//old vanilla node.js
-// const server = http.createServer(routes);
+app.use(adminRoutes);
 
-const server = http.createServer(app);
+app.use(shopRoutes);
 
-
-server.listen(3000);
-//sdfasdf
+app.listen(4000);
