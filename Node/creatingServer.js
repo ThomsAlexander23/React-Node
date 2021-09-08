3f1d46812e26ea71d45e8c63369d29bd220bdf5b
Alex <thoms.alexander23@gmail.com>
2021-09-08 18:50:05 -0400

    feat: added file writing feature to store data in a file; refactored to eliminate repetitivness and increase readability

--- Node/creatingServer.js
+++ Node/creatingServer.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 // const expressHbs = require('express-handlebars');
 
+const errorController = require('./controllers/errors.js');
+
 const app = express();
 
 // app.engine('hbs', expressHbs({
@@ -17,18 +19,15 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle: '404 error'});
-});
+app.use(errorController.get404)
 
 app.listen(4000);
